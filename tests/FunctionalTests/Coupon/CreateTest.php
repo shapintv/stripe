6f102fd46d17f7473d42cf01bf427fc844f850a0
Olivier Dolbeau <git@a.bbnt.me>
2019-08-12 16:19:40 +0200

    Split coupon test

---
+++ tests/FunctionalTests/Coupon/CreateTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+namespace Shapin\Stripe\Tests\FunctionalTests\Coupon;
+
+use Shapin\Stripe\Model\Coupon\Coupon;
+use Shapin\Stripe\Model\MetadataCollection;
+use Shapin\Stripe\Tests\FunctionalTests\TestCase;
+
+final class CreateTest extends TestCase
+{
+    private $couponApi;
+
+    public function setUp(): void
+    {
+        $this->couponApi = $this->getStripeClient()->coupons();
+    }
+
+    public function testCreateCoupon()
+    {
+        $coupon = $this->couponApi->create([
+            'duration' => 'once',
+            'amount_off' => 500,
+            'currency' => 'EUR',
+            'name' => 'a test coupon',
+        ]);
+
+        $this->assertInstanceOf(Coupon::class, $coupon);
+    }
+}
